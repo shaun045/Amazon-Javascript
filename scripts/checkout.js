e9786866e24e93a8b5762543c482964630747d02
shaun045 <shaun1adam2@gmail.com>
2026-01-16 03:53:38 +0800

    Load Products from the backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,10 @@
 import { renderOrderSummary } from "./checkout/ordersummary.js";
 import { renderPaymentSummary } from "./checkout/paymentsummary.js";
+import { loadProdcuts } from "../data/products.js";
 // import '../data/cart-class.js';
-import '../data/backend-practice.js';
+// import '../data/backend-practice.js';
 
-renderOrderSummary();
-renderPaymentSummary();
+loadProdcuts(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+});
