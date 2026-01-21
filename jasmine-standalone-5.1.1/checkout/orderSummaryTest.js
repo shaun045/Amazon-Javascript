1f3dabb71fa3a3df1202decebe91075bf9b2578d
shaun045 <shaun1adam2@gmail.com>
2026-01-22 03:00:28 +0800

    Add URL parameters example

--- jasmine-standalone-5.1.1/checkout/orderSummaryTest.js
+++ jasmine-standalone-5.1.1/checkout/orderSummaryTest.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from "../../scripts/checkout/ordersummary.js";
 import { loadFromStorage, cart } from "../../data/cart.js";
-import { loadProdcuts, loadProdcutsFetch } from "../../data/products.js";
+import { loadProducts, loadProductsFetch } from "../../data/products.js";
 
 
 describe('test suite: renderOrderSummary', () => {
@@ -8,7 +8,7 @@ describe('test suite: renderOrderSummary', () => {
   const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
 
   beforeAll((done) => {
-    loadProdcutsFetch().then(() => {
+    loadProductsFetch().then(() => {
       done();
     });
   });
